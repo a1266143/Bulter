5f54875be6d7d9338bfc3785580222878d89fef5
xiaojun.li <603820467@qq.com>
2017-05-25 16:49:40 +0800

    优化网络框架

--- bulter/src/main/java/com/xiaojun/bulter/NetUtils.java
+++ bulter/src/main/java/com/xiaojun/bulter/NetUtils.java
@@ -1,11 +1,14 @@
 package com.xiaojun.bulter;
 
+import com.google.gson.Gson;
 import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.Callback;
 import com.zhy.http.okhttp.callback.StringCallback;
 
 import java.util.Map;
 
 import okhttp3.Call;
+import okhttp3.Response;
 
 /**
  * 网络请求工具类
@@ -15,24 +18,33 @@ import okhttp3.Call;
 public class NetUtils {
     /**
      * Get请求
+     *
      * @param url
      * @param params
      */
-    public static void doGet(String url, Map<String,String> params, final NetUtilsCallback callback){
-        OkHttpUtils.get().url(url).params(params).build().execute(new StringCallback() {
+    public static <T> void doGet(String url, Map<String, String> params, final NetUtilsCallback<T> callback, final T beanType) {
+        OkHttpUtils.get().url(url).params(params).build().execute(new Callback<T>() {
+            @Override
+            public T parseNetworkResponse(Response response, int id) throws Exception {
+                String string = response.body().string();
+                try {
+                    T t = (T) new Gson().fromJson(string, beanType.getClass());
+                    return t;
+                } catch (Exception e) {
+                    return null;
+                }
+
+
+            }
+
             @Override
             public void onError(Call call, Exception e, int id) {
                 callback.getNetError();
             }
 
             @Override
-            public void onResponse(final String response, int id) {
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        callback.getData(response);
-                    }
-                }).start();
+            public void onResponse(T response, int id) {
+                callback.getData(response);
             }
         });
 
@@ -40,32 +52,42 @@ public class NetUtils {
 
     /**
      * doPost请求
+     *
      * @param url
      * @param params
      * @param callback
      */
-    public static void doPost(String url,Map<String,String> params,final NetUtilsCallback callback){
-        OkHttpUtils.post().url(url).params(params).build().execute(new StringCallback() {
+    public static <T> void doPost(String url, Map<String, String> params, final NetUtilsCallback<T> callback, final T beanType) {
+
+        OkHttpUtils.post().url(url).params(params).build().execute(new Callback<T>() {
             @Override
-            public void onError(Call call, Exception e, int id) {
-                callback.getNetError();
+            public T parseNetworkResponse(Response response, int id) throws Exception {
+                String string = response.body().string();
+                try {
+                    T t = (T) new Gson().fromJson(string, beanType.getClass());
+                    return t;
+                } catch (Exception e) {
+                    return null;
+                }
+
             }
 
             @Override
-            public void onResponse(final String response, int id) {
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        callback.getData(response);
-                    }
-                }).start();
+            public void onError(Call call, Exception e, int id) {
+                        callback.getNetError();
+            }
+
+            @Override
+            public void onResponse(T response, int id) {
+                callback.getData(response);
             }
         });
     }
 
-    public interface NetUtilsCallback{
+    public interface NetUtilsCallback<T> {
         //获取到数据
-        void getData(String data);
+        void getData(T data);
+
         //网络错误
         void getNetError();
     }
