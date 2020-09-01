29d70fab25af96fbfaea92b2716c1f8ce7b04d7e
Yoman <603820467@qq.com>
2020-09-01 10:02:24 +0800

    简化工具类

--- bulter/src/main/java/com/xiaojun/bulter/yyUtils/YyScreenUtils.java
+++ bulter/src/main/java/com/xiaojun/bulter/yyUtils/YyScreenUtils.java
@@ -9,25 +9,14 @@ import android.view.WindowManager;
  * Created by xiaojun on 2017/11/22.
  */
 
-public class ScreenUtils {
-
-    private static ScreenUtils screenUtils;
-    private static Context context;
-    private ScreenUtils(Context context){
-        ScreenUtils.context = context;
-    }
-
-    public static ScreenUtils getInstance(Context context){
-        if (screenUtils == null)
-            screenUtils = new ScreenUtils(context);
-        return screenUtils;
-    }
+public class YyScreenUtils {
 
     /**
      * 获取屏幕宽度
+     *
      * @return
      */
-    public static int getScreenWidth(){
+    public static int getScreenWidth(Context context) {
         WindowManager manager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
         DisplayMetrics outMetrics = new DisplayMetrics();
         manager.getDefaultDisplay().getMetrics(outMetrics);
@@ -37,9 +26,10 @@ public class ScreenUtils {
 
     /**
      * 获取屏幕高度
+     *
      * @return
      */
-    public static int getScreenHeight(){
+    public static int getScreenHeight(Context context) {
         WindowManager manager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
         DisplayMetrics outMetrics = new DisplayMetrics();
         manager.getDefaultDisplay().getMetrics(outMetrics);
@@ -49,20 +39,22 @@ public class ScreenUtils {
 
     /**
      * dp转px
+     *
      * @param dpValue dp值
      * @return 返回像素值
      */
-    public static int dp2px(float dpValue) {
+    public static int dp2px(Context context, float dpValue) {
         final float scale = context.getResources().getDisplayMetrics().density;
         return (int) (dpValue * scale + 0.5f);
     }
 
     /**
      * px转dp
+     *
      * @param pxValue 像素值
      * @return 返回dp值
      */
-    public static int px2dp(float pxValue) {
+    public static int px2dp(Context context, float pxValue) {
         final float scale = context.getResources().getDisplayMetrics().density;
         return (int) (pxValue / scale + 0.5f);
     }
--- bulter/src/main/java/com/xiaojun/bulter/yyUtils/YySpUtils.java
+++ bulter/src/main/java/com/xiaojun/bulter/yyUtils/YySpUtils.java
@@ -2,22 +2,19 @@ package com.xiaojun.bulter.yyUtils;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.os.Build;
-
-import java.util.Set;
 
 /**
  * SharedPreferences工具类
  * Created by xiaojun on 2017/11/22.
  */
 
-public class SpUtils {
+public class YySpUtils {
 
-    private static SpUtils spUtils;
+    private static YySpUtils yySpUtils;
     private static SharedPreferences sp;
     private static SharedPreferences.Editor editor;
 
-    private SpUtils(Context context, String spName) {
+    private YySpUtils(Context context, String spName) {
         sp = context.getSharedPreferences(spName, Context.MODE_PRIVATE);
         editor = sp.edit();
     }
@@ -29,13 +26,19 @@ public class SpUtils {
      * @param spName  SharedPreferences文件名称
      * @return 返回当前对象
      */
-    static SpUtils getInstance(Context context, String spName) {
-        if (spUtils == null)
-            spUtils = new SpUtils(context, spName);
-        return spUtils;
+    public static YySpUtils getInstance(Context context, String spName) {
+        if (yySpUtils == null)
+            yySpUtils = new YySpUtils(context, spName);
+        return yySpUtils;
     }
 
-    /*public static Object get(String key, Object defaultValue) {
+    /**
+     * 获取SharedPreference中的对应key的value
+     * @param key
+     * @param defaultValue
+     * @return
+     */
+    public static Object get(String key, Object defaultValue) {
         if (defaultValue instanceof String)
             return sp.getString(key, (String) defaultValue);
         else if (defaultValue instanceof Boolean)
@@ -48,8 +51,13 @@ public class SpUtils {
             return sp.getLong(key, (Long) defaultValue);
         else
             return null;
-    }*/
+    }
 
+    /**
+     * 保存key-value到SharedPreference中
+     * @param key
+     * @param defaultValue
+     */
     public static void put(String key, Object defaultValue) {
         if (defaultValue instanceof String)
             editor.putString(key, (String) defaultValue);
@@ -64,28 +72,4 @@ public class SpUtils {
         editor.apply();
     }
 
-
-    public static <T> T get(String key, Class<T> clazz) {
-        if (null == sp) {
-            return null;
-        }
-        if ((String.class).equals(clazz)) {
-            return (T) sp.getString(key, null);
-        } else if ((Integer.class).equals(clazz)) {
-            return (T) Integer.valueOf(sp.getInt(key, Integer.MIN_VALUE));
-        } else if ((Float.class).equals(clazz)) {
-            return (T) Float.valueOf(sp.getFloat(key, Integer.MIN_VALUE));
-        } else if ((Long.class).equals(clazz)) {
-            return (T) Long.valueOf(sp.getLong(key, Integer.MIN_VALUE));
-        } else if ((Boolean.class).equals(clazz)) {
-            return (T) Boolean.valueOf(sp.getBoolean(key, false));
-        } else if ((Set.class).equals(clazz)) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-                return (T) sp.getStringSet(key, null);
-            } else {
-                System.out.println(Build.VERSION.SDK_INT + "is to low,get data failed");
-            }
-        }
-        return null;
-    }
 }
